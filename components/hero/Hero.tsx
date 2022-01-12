79b123f2ba59213672acc255e5e32490450104a3
Matheus Silva <MSilva@synergyonline.com>
2022-01-12 13:59:16 -0500

    updating introduction paragraph

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
                 <Image src={'/images/site/matheus.png'} alt="An image showing Matheus" width={300} height={300} />
             </div>
             <h1>Hi, I am Matheus</h1>
-            <p>I blog about web development - especially frontend framework like Angular or React.</p>
+            <p>I blog about web development - especially JavaScript frameworks and libraries.</p>
         </section>
     );
 };
